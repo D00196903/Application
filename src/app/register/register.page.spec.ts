463b71a176143806814b5086500edda7315018df
D00196903 <82539995+D00196903@users.noreply.github.com>
2023-05-05 19:29:15 +0100

    initialize commit
    made chages to the look of the application

--- src/app/register/register.page.spec.ts
+++ src/app/register/register.page.spec.ts
@@ -1,45 +1,24 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
-import { AngularFireAuth } from '@angular/fire/compat/auth';
-import { LoadingController, NavController, ToastController } from '@ionic/angular';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
+import { IonicModule } from '@ionic/angular';
+
 import { RegisterPage } from './register.page';
 
 describe('RegisterPage', () => {
   let component: RegisterPage;
   let fixture: ComponentFixture<RegisterPage>;
 
-  let toastCtrlSpy: jasmine.SpyObj<ToastController>;
-  let loadingCtrlSpy: jasmine.SpyObj<LoadingController>;
-  let afAuthSpy: jasmine.SpyObj<AngularFireAuth>;
-  let navCtrlSpy: jasmine.SpyObj<NavController>;
-
-  beforeEach(async () => {
-    const toastSpy = jasmine.createSpyObj('ToastController', ['create']);
-    const loadingSpy = jasmine.createSpyObj('LoadingController', ['create']);
-    const afAuthSpyObj = jasmine.createSpyObj('AngularFireAuth', ['createUserWithEmailAndPassword']);
-    const navCtrlSpyObj = jasmine.createSpyObj('NavController', ['navigateRoot']);
-
-    await TestBed.configureTestingModule({
+  beforeEach(waitForAsync(() => {
+    TestBed.configureTestingModule({
       declarations: [ RegisterPage ],
-      providers: [
-        { provide: ToastController, useValue: toastSpy },
-        { provide: LoadingController, useValue: loadingSpy },
-        { provide: AngularFireAuth, useValue: afAuthSpyObj },
-        { provide: NavController, useValue: navCtrlSpyObj }
-      ]
+      imports: [IonicModule.forRoot()]
     }).compileComponents();
-
-    toastCtrlSpy = TestBed.inject(ToastController) as jasmine.SpyObj<ToastController>;
-    loadingCtrlSpy = TestBed.inject(LoadingController) as jasmine.SpyObj<LoadingController>;
-    afAuthSpy = TestBed.inject(AngularFireAuth) as jasmine.SpyObj<AngularFireAuth>;
-    navCtrlSpy = TestBed.inject(NavController) as jasmine.SpyObj<NavController>;
 
     fixture = TestBed.createComponent(RegisterPage);
     component = fixture.componentInstance;
     fixture.detectChanges();
-  });
+  }));
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
-
 });
